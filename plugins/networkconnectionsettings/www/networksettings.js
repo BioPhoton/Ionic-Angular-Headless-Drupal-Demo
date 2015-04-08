de7f0f9744a409c8d22f4edd4b6c60fe700e7aca
Michael Hladky <michael@hladky.at>
2015-04-08 16:40:47 +0200

    edit

---
+++ plugins/networkconnectionsettings/www/networksettings.js
@@ -0,0 +1,17 @@
+var calendarPlugin = {
+    createEvent: function(title, location, notes, startDate, endDate, successCallback, errorCallback) {
+        cordova.exec(
+            successCallback, // success callback function
+            errorCallback, // error callback function
+            'CalendarPlugin', // mapped to our native Java class called "CalendarPlugin"
+            'addCalendarEntry', // with this action name
+            [{                  // and this array of custom arguments to create our entry
+                "title": title,
+                "description": notes,
+                "eventLocation": location,
+                "startTimeMillis": startDate.getTime(),
+                "endTimeMillis": endDate.getTime()
+            }]
+        );
+     }
+}
