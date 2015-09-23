a748e9ea8a77ff9e1ce50d1816aad77f9027946b
Michael Hladky <michael@hladky.at>
2015-09-23 17:58:20 +0200

    edit

--- www/app/app.js
+++ www/app/app.js
@@ -111,7 +111,7 @@ drupalIonicAngularJSAPIClient
 		
 		//Configure ng-drupal-ionic
 		//edit drupal config
-		drupalApiConfig.drupal_instance = 'http://www.drupalionic.org/backend/';
+		drupalApiConfig.drupal_instance = 'http://www.drupalionic.org/drupal_demo/';
 		drupalApiConfig.api_endpoint += 'v1/';
 		
 		//edit accessControl config
